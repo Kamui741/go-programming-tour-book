51839200861bdc1605e33848277a855e6ac0eaed
ChZheng <2215702430@qq.com>
2022-01-31 00:37:55 +0800

    3.6.4.2.2

--- tag-service/client/client.go
+++ tag-service/client/client.go
@@ -1,7 +1,7 @@
 /*
  * @Author: ChZheng
  * @Date: 2022-01-24 23:36:12
- * @LastEditTime: 2022-01-24 23:50:56
+ * @LastEditTime: 2022-01-25 22:48:42
  * @LastEditors: ChZheng
  * @Description:
  * @FilePath: /tag-service/client/client.go
@@ -10,12 +10,24 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
+	"net"
+	"net/http"
 
 	pb "go-programming-tour-book/tag-service/proto"
+	"go-programming-tour-book/tag-service/server"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
+
+var port string
+
+func init() {
+	flag.StringVar(&port, "port", "8003", "启动端口号")
+	flag.Parse()
+}
 
 func main() {
 	ctx := context.Background()
@@ -31,4 +43,25 @@ func main() {
 func GetClientConn(ctx context.Context, target string, opts []grpc.DialOption) (*grpc.ClientConn, error) {
 	opts = append(opts, grpc.WithInsecure())
 	return grpc.DialContext(ctx, target, opts...)
+}
+func RunTcpServer(port string) (net.Listener, error) {
+	return net.Listen("tcp", ":"+port)
+}
+
+func RunGrpcServer() *grpc.Server {
+	s := grpc.NewServer()
+	pb.RegisterTagServiceServer(s, server.NewTagServer())
+	reflection.Register(s)
+	return s
+}
+
+func RunHttpServer(port string) *http.Server {
+	serverMux := http.NewServeMux()
+	serverMux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte(`pong`))
+	})
+	return &http.Server{
+		Addr:    ":" + port,
+		Handler: serverMux,
+	}
 }
--- tag-service/go.mod
+++ tag-service/go.mod
@@ -4,13 +4,18 @@ go 1.17
 
 require (
 	github.com/golang/protobuf v1.5.2
-	google.golang.org/grpc v1.43.0
+	github.com/grpc-ecosystem/grpc-gateway v1.14.5
+	google.golang.org/grpc v1.39.0-dev
 )
 
 require (
-	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
-	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
-	golang.org/x/text v0.3.0 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/golang/glog v1.0.0 // indirect
+	github.com/soheilhy/cmux v0.1.4 // indirect
+	golang.org/x/net v0.0.0-20220121210141-e204ce36a2ba // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
-	google.golang.org/protobuf v1.26.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
